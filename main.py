13e59521164918e11d7a84c4196b07e5701c59ec
elynnne <eylulnarkuz@gmail.com>
2026-06-25 11:43:35 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+
+API_KEY = "514854dcd25062c8b6309fd16ae3e34c"
+city = input("Enter city name: ")
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+response = requests.get(url)
+data=response.json()
+
+if data["cod"] == 200:
+    temp = data["main"]["temp"]
+    feels_like = data["main"]["feels_like"]
+    humidity= data["main"]["humidity"]
+    description = data["weather"][0]["description"]
+
+    print(f"\n📍 Weather in {city}:")
+    print(f"🌡️ Temperature: {temp}°C")
+    print(f"🌬️  Feels like: {feels_like}°C")
+    print(f"💧 Humidity: {humidity}%")
+    print(f"☁️ Description: {description}")
+else:
+    print("City not found try again please.")

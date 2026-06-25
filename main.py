4d8521b6269fe3c2d2e706b16513ed07fb602a3f
elynnne <eylulnarkuz@gmail.com>
2026-06-25 13:46:12 +0300

    add modern UI design

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
+from flask import Flask, render_template, request
 import requests
 
+app = Flask(__name__)
+
 API_KEY = "514854dcd25062c8b6309fd16ae3e34c"
-city = input("Enter city name: ")
-url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-response = requests.get(url)
-data=response.json()
 
-if data["cod"] == 200:
-    temp = data["main"]["temp"]
-    feels_like = data["main"]["feels_like"]
-    humidity= data["main"]["humidity"]
-    description = data["weather"][0]["description"]
+@app.route("/", methods=["GET", "POST"])
+def index():
+    weather = None
+    forecast = []
+    city = None
+    error = None
 
-    print(f"\n📍 Weather in {city}:")
-    print(f"🌡️ Temperature: {temp}°C")
-    print(f"🌬️  Feels like: {feels_like}°C")
-    print(f"💧 Humidity: {humidity}%")
-    print(f"☁️ Description: {description}")
+    if request.method == "POST":
+        city = request.form["city"]
 
-    forecast_url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}&units=metric&cnt=5"
-    forecast_response = requests.get(forecast_url)
-    forecast_data = forecast_response.json()
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+        response = requests.get(url)
+        data = response.json()
 
-    print(f"\n📅 5-Day Forecast for {city}:")
-    for item in forecast_data["list"]:
-        date = item["dt_txt"]
-        temp_f = item["main"]["temp"]
-        desc_f= item["weather"][0]["description"]
-        print(f"{date}: {temp_f}°C, {desc_f}")
-else:
-    print("City not found try again please.")
+        if data["cod"] == 200:
+            weather = {
+                "city": city,
+                "temp": round(data["main"]["temp"]),
+                "feels_like": round(data["main"]["feels_like"]),
+                "humidity": data["main"]["humidity"],
+                "description": data["weather"][0]["description"],
+                "wind": round(data["wind"]["speed"] * 3.6)
+            }
+
+            forecast_url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}&units=metric&cnt=5"
+            forecast_response = requests.get(forecast_url)
+            forecast_data = forecast_response.json()
+
+            for item in forecast_data["list"]:
+                forecast.append({
+                    "date": item["dt_txt"][5:16],
+                    "temp": round(item["main"]["temp"]),
+                    "desc": item["weather"][0]["description"]
+                })
+        else:
+            error = True
+
+    return render_template("index.html", weather=weather, forecast=forecast, city=city, error=error)
+
+if __name__ == "__main__":
+    app.run(debug=True)
